f5c736e644a1f5fb9a320da5fae621f4eefa1c42
pattenbranden <patten.branden@gmail.com>
2020-02-01 15:18:38 -0500

    adding initial files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA4F1E0xmpl_LHfyFT8dKriyQq0GFmzFtw",
+    authDomain: "train-schedule-34696.firebaseapp.com",
+    databaseURL: "https://train-schedule-34696.firebaseio.com",
+    projectId: "train-schedule-34696",
+    storageBucket: "train-schedule-34696.appspot.com",
+    messagingSenderId: "985738958694",
+    appId: "1:985738958694:web:f86876d559ed730a0f9aa7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var train;
+  var trainLine
